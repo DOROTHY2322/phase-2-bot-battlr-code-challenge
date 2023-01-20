5ad069b3c19b776c54663370c9f9f05ae816e5a9
DOROTHY2322 <kimelidorothy@gmail.com>
2023-01-20 16:57:43 +0300

    displaying  bots

--- src/components/BotCollection.js
+++ src/components/BotCollection.js
@@ -5,6 +5,14 @@ import React, {useEffect, useState} from "react";
 const BotCollection = () => {
 
     const [bots, setBots] = useState([]);
+    const [selectedBots, setSelectedBots] = useState([]);
+
+const handleBotClick = (bot) => {
+    if(!selectedBots.find(b => b.id === bot.id)){
+        setSelectedBots([...selectedBots, bot])
+    }
+}
+
 
     useEffect(() => {
         fetch("https://json-server-vercel-34ln.vercel.app/api/bots")
@@ -16,7 +24,7 @@ const BotCollection = () => {
         <div>
             <h1>Bot Collection</h1>
             {bots.map((bot) => (
-                <div key={bot.id}>
+                <div key={bot.id} className="bot" onClick={() => handleBotClick(bot)}>
                     <h2>{bot.name}</h2>
                     <p>Health: {bot.health}</p>
                     <p>Damage: {bot.damage}</p>
