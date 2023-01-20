4311a5aaf8b8de5058d654b8110eb41c96ea1858
DOROTHY2322 <kimelidorothy@gmail.com>
2023-01-20 16:12:44 +0300

    rendering data

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import BotCollection from './BotCollection';
+
+function App() {
+  return (
+    <div>
+<h2>ROBOT ARMY</h2>
+<BotCollection/>
+    </div>
+  );
+}
+
+export default App;
