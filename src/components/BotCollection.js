22141e39e551a101d18e7d25ffedd3decf0f2e66
DOROTHY2322 <kimelidorothy@gmail.com>
2023-01-20 16:33:48 +0300

    displaying bots

--- src/components/BotCollection.js
+++ src/components/BotCollection.js
@@ -14,9 +14,21 @@ const BotCollection = () => {
     console.log(bots)
     return(
         <div>
-
+            <h1>Bot Collection</h1>
+            {bots.map((bot) => (
+                <div key={bot.id}>
+                    <h2>{bot.name}</h2>
+                    <p>Health: {bot.health}</p>
+                    <p>Damage: {bot.damage}</p>
+                    <p>Armor: {bot.armor}</p>
+                    <p>Class: {bot.bot_class}</p>
+                    <p>Catchphrase: {bot.catchphrase}</p>
+                    <img src={bot.avatar_url} alt={bot.name}/>
+                </div>
+            ))}
         </div>
     )
+    
 
 }
 export default BotCollection;
