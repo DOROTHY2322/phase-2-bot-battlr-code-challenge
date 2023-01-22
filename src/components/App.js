6767c0d77eabdc163307d38cfe127ae22864c980
DOROTHY2322 <kimelidorothy@gmail.com>
2023-01-22 12:41:18 +0300

    components

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import './App.css';
-import BotCollection from './BotCollection';
+
 
 function App() {
   return (
     <div>
-<h2>ROBOT ARMY</h2>
-<BotCollection/>
+
     </div>
   );
 }
