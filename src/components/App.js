3b8e1e47672bb6e538e27faddd28699c68398db2
DOROTHY2322 <kimelidorothy@gmail.com>
2023-01-22 13:07:46 +0300

    displaying bots

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import './App.css';
-
+import YourBotArmy from './YourBotArmy';
+import BotCollection from './BotCollection';
 
 function App() {
   return (
     <div>
-
+<h2 className='navbar navbar-expand-lg bg-body-secondary'>ROBOT ARMY</h2>
+<BotCollection/>
+<YourBotArmy/>
     </div>
   );
 }
--- src/components/YourBotArmy.js
+++ src/components/YourBotArmy.js
@@ -1,1 +1,27 @@
+import React, { useState } from "react";
 
+function YourBotArmy() {
+    const [selectedBots, setSelectedBots] = useState([]);
+
+   
+
+    // JSX to render the selected bots
+    return (
+        <div>
+            {selectedBots.map((bot) => (
+                <div key={bot.id} className="bot">
+                    <img src={bot.avatar_url} className="img-fluid rounded float-start" alt={bot.name}/>
+                    <h2>{bot.name}</h2>
+                    <p>Health: {bot.health}</p>
+                    <p>Damage: {bot.damage}</p>
+                    <p>Armor: {bot.armor}</p>
+                    <p>Class: {bot.bot_class}</p>
+                    <p>Catchphrase: {bot.catchphrase}</p>
+                
+                </div>
+            ))}
+        </div>
+    )
+}
+
+export default YourBotArmy;
