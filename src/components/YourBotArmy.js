8ef15414c35e2e6663deab4494d1e43443628de6
DOROTHY2322 <kimelidorothy@gmail.com>
2023-01-22 17:43:07 +0300

    event listeners

--- src/components/YourBotArmy.js
+++ src/components/YourBotArmy.js
@@ -1,25 +1,32 @@
-import React, { useState } from "react";
+import React from "react";
 
 
-function YourBotArmy() {
-    const [selectedBots] = useState([]);  
+const YourBotArmy = ({selectedBots}) => {
     return (
         <div>
-           
-            {selectedBots.map((bot) => (
-                <div key={bot.id} className="bot">
-                    <img src={bot.avatar_url} className="img-fluid rounded float-start" alt={bot.name}/>
-                    <h2>{bot.name}</h2>
-                    <p>Health: {bot.health}</p>
-                    <p>Damage: {bot.damage}</p>
-                    <p>Armor: {bot.armor}</p>
-                    <p>Class: {bot.bot_class}</p>
-                    <p>Catchphrase: {bot.catchphrase}</p>
-                
-                </div>
-            ))}
+            <h1>Your Bot Army</h1>
+            <div className="selected-bot-container">
+                {selectedBots ? (
+                    selectedBots.map((bot) => (
+                        <div key={bot.id} className="selected-bot">
+                            <img src={bot.avatar_url} className="img-fluid rounded float-start" alt={bot.name}/>
+                            <h2>{bot.name}</h2>
+                            <p>Health: {bot.health}</p>
+                            <p>Damage: {bot.damage}</p>
+                            <p>Armor: {bot.armor}</p>
+                            <p>Class: {bot.bot_class}</p>
+                            <p>Catchphrase: {bot.catchphrase}</p>
+                        </div>
+                    ))
+                ) : (
+                    <div>You have not selected any bots yet.</div>
+                )}
+            </div>
         </div>
-    )
-}
+    );
+};
+
+
+  
 
 export default YourBotArmy;
